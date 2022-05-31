1517fba36df4c05b04479fc26fe699a3cdd99dca
Pedro Thomaz Conzatti Martins <pedrothomazm@gmail.com>
2022-05-31 18:45:06 -0300

    Crie obtem_cotacoes()

--- cotacoes.py
+++ cotacoes.py
@@ -10,3 +10,16 @@ def moeda_em_real(moeda):
     ticker = yf.Ticker(texto_conversao)
     moeda_em_real = ticker.info["regularMarketPrice"]
     return moeda_em_real
+
+def obtem_cotacoes(acoes, dias):
+    texto_acoes = ' '.join(acoes)
+    tickers_acoes = yf.Tickers(texto_acoes)
+
+    dicionario_cotacoes = dict(tickers_acoes.tickers)
+    texto_historico = f"{dias}d"
+    for acao in dicionario_cotacoes.keys():
+        dicionario_cotacoes[acao] = dicionario_cotacoes[acao].history(texto_historico)
+    
+    return dicionario_cotacoes
+
+print(obtem_cotacoes(["NFLX", "AAPL"], 1))
