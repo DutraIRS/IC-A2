ec01516c1bbe3df08d80ddb2806f27d73a7bfeae
Pedro Thomaz Conzatti Martins <pedrothomazm@gmail.com>
2022-05-31 18:10:24 -0300

    Crie moeda_em_reais()

--- cotacoes.py
+++ cotacoes.py
@@ -2,3 +2,6 @@ import yfinance as yf
 
 def obtem_cotacao(acao, dias):
    return yf.Ticker(acao).history(f"{dias}d")
+
+def moeda_em_reais(moeda):
+    return yf.Ticker(f"{moeda}BRL=X").info["regularMarketPrice"]
