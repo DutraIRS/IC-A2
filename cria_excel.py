c95d02c0b3eed5711a641bc2875ddbd1aacd9715
josueeeeee <105641198+josueeeeee@users.noreply.github.com>
2022-06-16 15:22:12 -0300

    Add files via upload
    cria excel

---
+++ cria_excel.py
@@ -0,0 +1,45 @@
+import busca_carteira
+import openpyxl
+import pandas as pd
+import xlsxwriter
+
+def criar_excel(url):
+    carteira = busca_carteira.buscar_carteira(url)
+    #
+    ativos = list(carteira.items())
+    moedas = ativos[0]
+    acoes = ativos[1]
+
+    moedas_nomes_quantidades = moedas[1]
+    moedas_nomes = []
+    moedas_quantidades = []
+
+    for key, val in moedas_nomes_quantidades.items(): 
+        moedas_nomes.append(key) 
+    for val in moedas_nomes_quantidades.values():
+        moedas_quantidades.append(val)
+
+    acoes_nomes_quantidades = acoes[1]
+    acoes_nomes = []
+    acoes_quantidades = []
+
+    for key, val in acoes_nomes_quantidades.items(): 
+        acoes_nomes.append(key) 
+    for val in acoes_nomes_quantidades.values():
+        acoes_quantidades.append(val)
+    #openpyxl
+    wb = openpyxl.Workbook()
+    wb.create_sheet("Carteira")
+    wb.remove(wb['Sheet'])
+    wb.save("Carteira.xlsx")
+        
+
+    coluna_moedas = {"moedas": moedas_nomes, "quantidade": moedas_quantidades}
+    coluna_acoes = {"ações": acoes_nomes, "quantidade": acoes_quantidades}
+    df = pd.DataFrame(coluna_moedas)
+    da = pd.DataFrame(coluna_acoes)
+    writer = pd.ExcelWriter('Carteira.xlsx', engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='welcome', index=False,)
+    da.to_excel(writer, sheet_name='welcome', index=False, startcol=3)
+
+    writer.save()
